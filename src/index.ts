56f2cf11a540d45b4f4113a7a1d54ca7b55cacb3
Alex Knott <alextknott@gmail.com>
2022-11-22 18:44:35 +0000

    Create and getWordFrequency function

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,10 @@ interface WordObject {
 const wordFreqList: WordObject[] = JSON.parse(
   readFileSync(`${__dirname}/wordFreqList.txt`, "utf-8")
 );
+
+function getWordFrequency(word: string) {
+  const wordObject = wordFreqList.find((e) => e.word === word);
+  return wordObject?.freq;
+}
+
+export { getWordFrequency };
