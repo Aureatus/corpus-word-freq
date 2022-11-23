9f9930db37aa61d202ed29b41a5dd00938ccc39c
Alex Knott <alextknott@gmail.com>
2022-11-23 13:12:45 +0000

    Add test suite for getWordFrequency function

---
+++ src/tests/getWordFrequency.test.ts
@@ -0,0 +1,13 @@
+import { getWordFrequency } from "..";
+
+describe("Word Frequency Tests", () => {
+  test("Freq of than", () => {
+    expect(getWordFrequency("than")).toBe("1033");
+  });
+  test("Freq of facilitate", () => {
+    expect(getWordFrequency("facilitate")).toBe("10");
+  });
+  test("Freq of taken", () => {
+    expect(getWordFrequency("taken")).toBe("355");
+  });
+});
