3c16b6596c0fe3fe5a5b595a9cd0f8c37a549460
Alex Knott <alextknott@gmail.com>
2022-11-23 15:58:16 +0000

    Update tests

--- src/tests/getWordFrequency.test.ts
+++ src/tests/getWordFrequency.test.ts
@@ -2,12 +2,15 @@ import { getWordFrequency } from "..";
 
 describe("Word Frequency Tests", () => {
   test("Freq of than", () => {
-    expect(getWordFrequency("than")).toBe("1033");
+    expect(getWordFrequency("than")).toBe(1033);
   });
   test("Freq of facilitate", () => {
-    expect(getWordFrequency("facilitate")).toBe("10");
+    expect(getWordFrequency("facilitate")).toBe(10);
   });
   test("Freq of taken", () => {
-    expect(getWordFrequency("taken")).toBe("355");
+    expect(getWordFrequency("taken")).toBe(355);
+  });
+  test("Freq of catallaxy", () => {
+    expect(getWordFrequency("catallaxy")).toBe(undefined);
   });
 });
