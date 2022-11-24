a4a10ef8f71e9fbbc48022fe71bd4d3488927f36
Alex Knott <alextknott@gmail.com>
2022-11-24 16:03:12 +0000

    Update implementation to accommodate removal of certain PoS

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,32 @@ interface WordObject {
   freq: string;
   PoS: string;
 }
+/* 
+PLAN
 
-const wordFreqList: WordObject[] = JSON.parse(
-  readFileSync(`${__dirname}/wordFreqList.txt`, "utf-8")
-);
+Initialise corpus object, with options for classifications of words to remove. NoC, NoP, Adj etc.
 
-function getWordFrequency(word: string) {
-  const wordObject = wordFreqList.find((e) => e.word === word);
-  const wordFreq = wordObject?.freq;
-  const wordFreqNumber = Number(wordFreq);
-  return wordFreqNumber || undefined;
-}
+Corpus Object contains methods to be run on that corpus.
 
-export { getWordFrequency };
+*/
+
+const corpusObject = (posToRemove: string[] | null = null) => {
+  let wordFreqList: WordObject[] = JSON.parse(
+    readFileSync(`${__dirname}/wordFreqList.txt`, "utf-8")
+  );
+
+  wordFreqList = wordFreqList.filter(
+    (wordObject: WordObject) => !posToRemove?.includes(wordObject.PoS)
+  );
+
+  const getWordFrequency = (word: string) => {
+    const wordObject = wordFreqList.find((e) => e.word === word);
+    const wordFreq = wordObject?.freq;
+    const wordFreqNumber = Number(wordFreq);
+    return wordFreqNumber || undefined;
+  };
+
+  return { getWordFrequency };
+};
+
+export { corpusObject };
