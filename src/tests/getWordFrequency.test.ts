f97f32c07bc23afa0780690dc9518f90d5fedf0b
Alex Knott <alextknott@gmail.com>
2022-11-24 16:03:22 +0000

    Update tests for new implementation

--- src/tests/getWordFrequency.test.ts
+++ src/tests/getWordFrequency.test.ts
@@ -1,16 +1,33 @@
-import { getWordFrequency } from "..";
+import { corpusObject } from "..";
 
-describe("Word Frequency Tests", () => {
+describe("Word Frequency Tests without any removals", () => {
+  const testing = corpusObject();
   test("Freq of than", () => {
-    expect(getWordFrequency("than")).toBe(1033);
+    expect(testing.getWordFrequency("than")).toBe(1033);
   });
   test("Freq of facilitate", () => {
-    expect(getWordFrequency("facilitate")).toBe(10);
+    expect(testing.getWordFrequency("facilitate")).toBe(10);
   });
   test("Freq of taken", () => {
-    expect(getWordFrequency("taken")).toBe(355);
+    expect(testing.getWordFrequency("taken")).toBe(355);
   });
   test("Freq of catallaxy", () => {
-    expect(getWordFrequency("catallaxy")).toBe(undefined);
+    expect(testing.getWordFrequency("catallaxy")).toBe(undefined);
+  });
+});
+
+describe("Word Frequency Tests with Conj removed", () => {
+  const testing = corpusObject(["Conj"]);
+  test("Freq of than", () => {
+    expect(testing.getWordFrequency("than")).toBe(undefined);
+  });
+  test("Freq of facilitate", () => {
+    expect(testing.getWordFrequency("facilitate")).toBe(10);
+  });
+  test("Freq of taken", () => {
+    expect(testing.getWordFrequency("taken")).toBe(355);
+  });
+  test("Freq of catallaxy", () => {
+    expect(testing.getWordFrequency("catallaxy")).toBe(undefined);
   });
 });
