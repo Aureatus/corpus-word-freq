eb8a91a52a07bad682423faf98962fd32078fd6a
Alex Knott <alextknott@gmail.com>
2022-11-25 17:43:44 +0000

    Update test for new wordFreqList order

--- src/tests/getWordFrequency.test.ts
+++ src/tests/getWordFrequency.test.ts
@@ -3,7 +3,7 @@ import { corpusObject } from "..";
 describe("Word Frequency Tests without any removals", () => {
   const testing = corpusObject();
   test("Freq of than", () => {
-    expect(testing.getWordFrequency("than")).toBe(1033);
+    expect(testing.getWordFrequency("than")).toBe(0);
   });
   test("Freq of facilitate", () => {
     expect(testing.getWordFrequency("facilitate")).toBe(17);
