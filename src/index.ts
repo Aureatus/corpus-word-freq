6edd008462e9d887e64112ab5d598ea81b4d34f2
Alex Knott <alextknott@gmail.com>
2022-11-26 18:36:49 +0000

    Create function to return matched words in order of freq from given parameters

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,28 @@ const corpusObject = (posToRemove: string[] | null = null) => {
     return typeof wordFreqNumber === "number" ? wordFreqNumber : undefined;
   };
 
-  return { getWordFrequency };
+  const getMatchedWords = (wordList: string[], desiredMatches: number) => {
+    let index = 0;
+    const matchedWords: WordObject[] = [];
+
+    while (matchedWords.length < desiredMatches) {
+      const isWordDuplicate = matchedWords.some(
+        (wordObject) => wordFreqList[index].word === wordObject.word
+      );
+
+      if (isWordDuplicate) index++;
+
+      const matchedWordObject = wordList.find(
+        (word) => word === wordFreqList[index].word
+      );
+      if (matchedWordObject) matchedWords.push(wordFreqList[index]);
+
+      index++;
+    }
+    return matchedWords;
+  };
+
+  return { getWordFrequency, getMatchedWords };
 };
 
 export { corpusObject };
